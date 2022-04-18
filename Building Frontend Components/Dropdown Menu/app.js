d47c802fc9faef717ecdd2c6e1e2e21c8adc8bdb
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-04-18 23:12:13 +0300

    Dropdown Menu Base - Hiding submenu correctly

--- Building Frontend Components/Dropdown Menu/app.js
+++ Building Frontend Components/Dropdown Menu/app.js
@@ -1,15 +1,17 @@
-function menuItemEnter() {
+function showSubmenu() {
   const submenu = document.getElementsByClassName('menu__sub')[0];
   submenu.style.display = 'block';
 }
 
-function menuItemLeave() {
+function hideSubmenu() {
   const submenu = document.getElementsByClassName('menu__sub')[0];
   submenu.style.display = 'none';
 }
 
 const menuItems = document.getElementsByClassName('menu__main__item');
 for (const menuItem of menuItems) {
-  menuItem.onmouseenter = menuItemEnter;
-  menuItem.onmouseleave = menuItemLeave;
+  menuItem.onmouseenter = showSubmenu;
 }
+
+const menu = document.getElementsByClassName('menu')[0];
+menu.onmouseleave = hideSubmenu;
