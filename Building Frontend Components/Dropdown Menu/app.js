f6f61819dd2e626f02930c4b9816a5d2c38e5671
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-04-19 21:47:18 +0300

    Dropdown Menu Base - done

--- Building Frontend Components/Dropdown Menu/app.js
+++ Building Frontend Components/Dropdown Menu/app.js
@@ -1,6 +1,75 @@
+const HOST = 'server.com/';
+
+// Server
+
+function getCategories(data) {
+  if (data.category == 'top') {
+    if (data.menuItem == 'Motors') {
+      return ['Car', 'Motorcycle', 'Plane', 'Trucks', 'Wheels'];
+    }
+    if (data.menuItem == 'Fashion') {
+      return ["Women's tops", "Men's tops", 'Jeans', 'Hats'];
+    }
+    return ['Server apple', 'Server banana', 'Server pear', 'Server orange'];
+  }
+  if (data.category == 'additional') {
+    if (data.menuItem == 'Motors') {
+      return ['Tires', 'Windshields', 'Ski racks', 'Doors', 'Windows'];
+    }
+    if (data.menuItem == 'Fashion') {
+      return ['On sale', 'Red stuff', 'Gucci', 'New Arrivals'];
+    }
+    return ['Server square', 'Server circle', 'Server oval', 'Server diamond'];
+  }
+  return [];
+}
+
+const endpoints = {
+  '/categories': {
+    get: getCategories,
+  },
+};
+
+function getFunction(url, data, callback) {
+  const domain = url.substring(0, url.indexOf('/'));
+  const endpoint = url.substring(url.indexOf('/'), url.length);
+
+  callback(endpoints[endpoint]['get'](data));
+}
+
+const api = {
+  get: getFunction,
+};
+
+function populateCategories(category) {
+  const activeMenuItemName = activeMenuItem.children[0].innerHTML;
+  api.get(
+    HOST + 'categories',
+    { category, menuItem: activeMenuItemName },
+    function (categories) {
+      let newCategories = '';
+      for (const category of categories) {
+        const categoryElement = `
+        <li class="menu__sub__categories__item">
+          <a href="#" class="menu__sub__categories__item__link">${category}</a>
+        </li>
+      `;
+        newCategories += categoryElement;
+      }
+      const categoriesElement = document.getElementsByClassName(
+        `menu__sub__categories__items--${category}`
+      )[0];
+      categoriesElement.innerHTML = newCategories;
+    }
+  );
+}
+
 function showSubmenu() {
   const submenu = document.getElementsByClassName('menu__sub')[0];
   submenu.style.display = 'block';
+
+  populateCategories('top');
+  populateCategories('additional');
 }
 
 function hideSubmenu() {
@@ -8,14 +77,16 @@ function hideSubmenu() {
   submenu.style.display = 'none';
 }
 
-let active = null;
+let activeMenuItem = null;
 
 function onMenuItemMouseEnter(item) {
-  if (active) {
-    active.classList.remove('menu__main__item--active');
+  if (activeMenuItem) {
+    activeMenuItem.classList.remove('menu__main__item--active');
+    activeMenuItem.classList.remove('menu__main__item--current');
   }
-  active = item;
+  activeMenuItem = item;
   item.classList.add('menu__main__item--active');
+  item.classList.add('menu__main__item--current');
   showSubmenu();
 }
 
@@ -26,3 +97,11 @@ for (const menuItem of menuItems) {
 
 const menu = document.getElementsByClassName('menu')[0];
 menu.onmouseleave = hideSubmenu;
+
+function deactivateMenuItem() {
+  console.log('Write your code here');
+  activeMenuItem.classList.remove('menu__main__item--active');
+}
+
+const submenu = document.getElementsByClassName('menu__sub')[0];
+submenu.onmouseleave = deactivateMenuItem;
