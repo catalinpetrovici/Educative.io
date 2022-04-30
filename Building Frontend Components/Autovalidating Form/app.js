01719a06f97df6ea48a06b7370f14a69b83dc104
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-04-30 23:30:12 +0300

    AutoValidating Form - Unique Errors for Each Field

--- Building Frontend Components/Autovalidating Form/app.js
+++ Building Frontend Components/Autovalidating Form/app.js
@@ -1,11 +1,21 @@
+function isValidName(name) {
+  // TODO
+}
+
+function isValidEmail(name) {
+  // TODO
+}
+
 function validate(event) {
   const inputElement = event.target;
   inputElement.classList.add('signup__field__input--error');
 
+  const field = inputElement.dataset.field;
+
   const errorMessageElement = event.target.parentElement.getElementsByClassName(
     'signup__field__error'
   )[0];
-  errorMessageElement.innerHTML = 'Sample Error';
+  errorMessageElement.innerHTML = `Error for ${field}`;
 }
 
 const inputs = document.getElementsByClassName('signup__field__input');
